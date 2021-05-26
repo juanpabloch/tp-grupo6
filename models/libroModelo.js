f2ba7a51f3806fd58b10fd09a473b31d460523dc
agustinmme <76716674+agustinmme@users.noreply.github.com>
2021-05-26 18:34:24 -0300

    put descripcion
    put descripcion

--- models/libroModelo.js
+++ models/libroModelo.js
@@ -4,22 +4,32 @@ class Libro{
 
     static estado(id){
         const query = 'SELECT persona_id FROM libro WHERE persona_id IS  NULL AND libro_id=?';
-        const respuesta = qy(query,[id]);
+        let respuesta = qy(query,[id]);
         return respuesta
     }
     static existe(id){
         const query = 'SELECT * FROM libro WHERE libro_id =?';
-        const respuesta = qy(query,[id]);
+        let respuesta = qy(query,[id]);
         return respuesta
     }
     static eliminar(id){
-        const query = 'DELETE FROM libro WHERE libro_id=?';
-        const respuesta = qy(query,[id]);
+        const query = 'DELETE FROM `libro` WHERE libro_id=?';
+        let respuesta = qy(query,[id]);
         return respuesta
     }
     static devolver(id){
         const query = 'UPDATE libro SET persona_id = NULL WHERE libro_id = ?';
-        const respuesta = qy(query,[id]);
+        let respuesta = qy(query,[id]);
+        return respuesta
+    }
+    static existe_plus(nombre,persona,categoria,id){
+        const query =  'SELECT * FROM libro WHERE nombre = ?   AND persona_id  = ?  AND categoria_id  = ? AND libro_id  = ?  ';
+        let respuesta = qy(query,[nombre,persona,categoria,id]);
+        return respuesta
+    }
+    static cambio_descripcion(nombre,descripcion,persona,categoria,id){
+        const query = 'UPDATE libro SET nombre = ?, descripcion = ?,persona_id  = ?, categoria_id = ? WHERE libro_id = ?';
+        let respuesta = qy(query,[nombre,descripcion,persona,categoria,id]);
         return respuesta
     }
 }
--- routes/rutaLibro.js
+++ routes/rutaLibro.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 //crear rutas
 //get('/') - get('/:id') - delete('/:id') - post('/') - put('/:id') - put('/prestar/:id') - put('/devolver/:id')
 
-
 //importamos controlador
-const controlador = require('../controllers/libroController')
+const controlador = require("../controllers/libroController");
 
-router.get('/',(req,res)=>{
-    res.send('Bienvenidos al Home');
- });
+router.get("/", (req, res) => {
+  res.send("Bienvenidos al Home").status(200);
+});
 
- router.delete("/:id",controlador.libro_borrar);
- router.put("/:id",controlador.libro_devolver);
+router.delete("/:id", controlador.libro_borrar);
+router.put("/devolver/:id", controlador.libro_devolver);
+router.put("/:id", controlador.libro_descripcion);
 module.exports = router;
