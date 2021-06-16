30720279cec9b4257761ec811ca17d678064576f
agustinmme <76716674+agustinmme@users.noreply.github.com>
2021-06-15 23:38:48 -0300

    vals
    vals

--- middleware/validaciones.js
+++ middleware/validaciones.js
@@ -13,12 +13,17 @@ const params = (req, res, next) => {
 const bodyCategoria = (req, res, next) => {
   const { nombre } = req.body;
   try {
+    nombre = nombre.replace(/  +/gi, " ");
+    nombre = nombre.trim();
+    if (!/^([^0-9]*)$/gi.test(nombre))
+      throw new Error("el nombre debe contener caracteres alfabeticos ");
     if (!/^[a-zA-ZÀ-ÿ]{2,}\s?(([a-zA-ZÀ-ÿ]{1,}\s?){1,3})/gi.test(nombre))
       throw new Error(
-        "el nombre debe contener solo caracteres 3 como minimo y deben ser alfabeticos "
+        "el nombre debe contener solo caracteres 3 como minimo y solo numeros alfabeticos"
       );
-      if (nombre.length > 70)
+    if (nombre.length > 70)
       throw new Error("el nombre no debe tener mas de 70 caracteres");
+    req.body.nombre = nombre;
     next();
   } catch (error) {
     next(error);
@@ -26,19 +31,31 @@ const bodyCategoria = (req, res, next) => {
 };
 
 const bodyPersona = (req, res, next) => {
-  const { nombre, apellido, alias, email } = req.body;
+  let { nombre, apellido, alias, email } = req.body;
   try {
     if (!nombre || !apellido || !alias || !email)
       throw new Error("Falta enviar datos");
+    nombre = nombre.replace(/  +/gi, " ");
+    apellido = apellido.replace(/  +/gi, " ");
+    alias = alias.replace(/  +/gi, " ");
+    email = email.replace(/  +/gi, " ");
+    nombre = nombre.trim();
+    apellido = apellido.trim();
+    alias = alias.trim();
+    email = email.trim();
+    if (!/^([^0-9]*)$/gi.test(nombre))
+      throw new Error("el nombre debe contener caracteres alfabeticos ");
     if (!/^[a-zA-ZÀ-ÿ]{2,}\s?(([a-zA-ZÀ-ÿ]{1,}\s?){1,3})/gi.test(nombre))
       throw new Error(
-        "el nombre debe contener solo caracteres 3 como minimo y deben ser alfabeticos "
+        "el nombre debe contener solo caracteres 3 como minimo y solo numeros alfabeticos"
       );
     if (nombre.length > 70)
       throw new Error("el nombre no debe tener mas de 70 caracteres");
+    if (!/^([^0-9]*)$/gi.test(apellido))
+      throw new Error("el nombre debe contener caracteres alfabeticos ");
     if (!/^[a-zA-ZÀ-ÿ]{2,}\s?(([a-zA-ZÀ-ÿ]{1,}\s?){1,3})/gi.test(apellido))
       throw new Error(
-        "el nombre debe contener solo caracteres 3 como minimo y deben ser alfabeticos "
+        "el apellido debe contener solo caracteres 3 como minimo y solo numeros alfabeticos"
       );
     if (apellido.length > 70)
       throw new Error("el apellido no debe tener mas de 70 caracteres");
@@ -48,6 +65,10 @@ const bodyPersona = (req, res, next) => {
       throw new Error("el alias debe contener solo caracteres de la a-z");
     if (!/^[a-z0-9_.]+@[a-z0-9]+\.[a-z0-9_.]+$/i.test(email))
       throw new Error("el email debe ser valido");
+    req.body.nombre = nombre;
+    req.body.apellido = apellido;
+    req.body.alias = alias;
+    req.body.email = email;
     next();
   } catch (err) {
     next(err);
@@ -58,24 +79,23 @@ const bodyLibroLong = (req, res, next) => {
   try {
     if (!req.body.nombre || !req.body.categoria_id)
       throw new Error("nombre y categoría son datos obligatorios");
-
     let { nombre, descripcion } = req.body;
-
     nombre = nombre.replace(/  +/gi, " ");
     nombre = nombre.trim();
-
+    if (!/^([^0-9]*)$/gi.test(nombre))
+      throw new Error("el nombre debe contener caracteres alfabeticos ");
     if (!/^[a-zA-ZÀ-ÿ]{2,}\s?(([a-zA-ZÀ-ÿ]{1,}\s?){1,15})/gi.test(nombre))
       throw new Error(
-        "el nombre debe contener solo caracteres 3 como minimo y deben ser alfabeticos "
+        "el nombre debe contener solo caracteres 3 como minimo y solo numeros alfabeticos"
       );
     if (nombre.length > 70)
       throw new Error("el nombre no debe tener mas de 70 caracteres");
     descripcion = descripcion.trim();
-    req.body.descripcion = descripcion.replace(/  +/gi, " ");
-
+    descripcion = descripcion.replace(/  +/gi, " ");
     if (descripcion.length > 200)
       throw new Error("la descripción no debe tener mas de 200 caracteres");
-
+    req.body.descripcion = descripcion;
+    req.body.nombre = nombre;
     next();
   } catch (err) {
     next(err);
