0ade22df6d6ad714925ab539d026999443df8644
juanpabloch <63684218+juanpabloch@users.noreply.github.com>
2021-07-20 02:06:00 -0300

    heroku

--- database/mysqlConnect.js
+++ database/mysqlConnect.js
@@ -2,18 +2,22 @@ const mysql = require('mysql');
 const util = require('util');
 
 const mysqlOptions = {
-    host: 'TU-HOST',
-    user: 'TU-USER',
-    password: 'TU-PASSWORD',
-    port:3306,
-    database: 'TU-DB'
+    host: 'us-cdbr-east-04.cleardb.com',
+    user: 'be222c4f1fd189',
+    password: 'b28eaa35',
+    database: 'heroku_bad93729dbe0353'
 }
-const connection = mysql.createConnection(mysqlOptions)
-connection.connect((err)=>{
-    if(err)throw new Error('Error al conectar a la base de datos');
-    console.log('base de datos mysql conectada')
-})
+const connection = mysql.createPool(mysqlOptions)
+
+
+// connection.connect((err)=>{
+//     if(err)throw new Error('Error al conectar a la base de datos');
+//     console.log('base de datos mysql conectada')
+// })
 
 const qy = util.promisify(connection.query).bind(connection)
 
 module.exports = qy;
+
+
+// mysql://be222c4f1fd189:b28eaa35@us-cdbr-east-04.cleardb.com/heroku_bad93729dbe0353?reconnect=true
