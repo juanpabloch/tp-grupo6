b35cdd5dad6de2fad34cb25004663b646bba1f49
JuanPablo <jchoternasty@the-8agency.com>
2023-08-16 13:32:07 -0300

    cambio en models query categoria 1

--- models/categoria.js
+++ models/categoria.js
@@ -2,7 +2,7 @@ const qy = require("../database/mysqlConnect");
 
 class Categoria {
   static async lista() {
-    const query = "SELECT * FROM categoria;";
+    const query = "SELECT * FROM categoria";
     let respuesta = await qy(query);
     return respuesta;
   }
@@ -16,31 +16,30 @@ class Categoria {
 
   static buscarNombre(nombre) {
     // const query = "SELECT nombre FROM categoria WHERE nombre = ?";
-    const query = "SELECT nombre FROM categoria WHERE nombre=$1;";
+    const query = "SELECT nombre FROM categoria WHERE nombre=$1";
     let respuesta = qy(query, [nombre]);
     return respuesta;
   }
 
   static async buscarCategoriaLibro(id) {
     // const query = "SELECT categoria_id FROM libro WHERE categoria_id = ?";
-    const query = "SELECT categoria_id FROM libro WHERE categoria_id=$1;";
+    const query = "SELECT categoria_id FROM libro WHERE categoria_id=$1";
     let respuesta = await qy(query, [id]);
     return respuesta;
   }
 
   static async buscar(id) {
     // const query = "SELECT * FROM categoria WHERE categoria_id = ?";
-    const query = "SELECT * FROM categoria WHERE categoria_id=$1;";
+    const query = "SELECT * FROM categoria WHERE categoria_id=$1";
     let respuesta = await qy(query, [id]);
     return respuesta;
   }
 
   static async agregar(categoria) {
-    // const query = "INSERT INTO categoria (nombre) VALUE (?)";
-    const query = "INSERT INTO categoria(nombre) VALUES($1);";
+    const query = "INSERT INTO categoria(nombre) VALUES($1)";
     let respuesta = await qy(query, [categoria]);
     return respuesta;
   }
 }
-
+// syntax error at or near \"VALUE\""
 module.exports = Categoria;
