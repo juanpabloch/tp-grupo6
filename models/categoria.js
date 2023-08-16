242328f047650b2838f8640180a7efcfb66d33e8
JuanPablo <jchoternasty@the-8agency.com>
2023-08-16 16:47:35 -0300

    cambios controller categoria fix

--- models/categoria.js
+++ models/categoria.js
@@ -4,7 +4,7 @@ class Categoria {
   static async lista() {
     const query = "SELECT * FROM categoria";
     let respuesta = await qy(query);
-    return respuesta.rows;
+    return respuesta;
   }
 
   static async eliminar(id) {
